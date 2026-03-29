f4ae3edadf4dd6dc0b5f780bc626ac0a78de8307
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2026-03-29 21:41:39 +0530

    Update config.py

--- Support_Files/config.py
+++ Support_Files/config.py
@@ -11,4 +11,11 @@
 VIRUSTOTAL_API_KEY  = "your_virustotal_api_key_here"
 ABUSEIPDB_API_KEY   = "your_abuseipdb_api_key_here"
 
+# ──────────────────────────────────────────────────────────
+# API URLS
+# ──────────────────────────────────────────────────────────
+
+VIRUSTOTAL_URL_SCAN     = "https://www.virustotal.com/api/v3/urls"
+VIRUSTOTAL_IP_SCAN      = "https://www.virustotal.com/api/v3/ip_addresses"
+ABUSEIPDB_URL           = "https://api.abuseipdb.com/api/v2/check"
 
