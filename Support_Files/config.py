a00cebbdd397817744c1d9c9e429109d7189ea8e
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2026-03-29 21:41:28 +0530

    Update config.py

--- Support_Files/config.py
+++ Support_Files/config.py
@@ -4,4 +4,11 @@
 # DO NOT push this file to GitHub
 # ============================================================
 
+# ──────────────────────────────────────────────────────────
+# API KEYS
+# ──────────────────────────────────────────────────────────
+
+VIRUSTOTAL_API_KEY  = "your_virustotal_api_key_here"
+ABUSEIPDB_API_KEY   = "your_abuseipdb_api_key_here"
+
 
