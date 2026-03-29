c03eb08c3302e8f7f65e9d1e22cb1c7921721e8e
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2026-03-29 19:56:15 +0530

    Update threat_intel.py

--- Backend_Files/threat_intel.py
+++ Backend_Files/threat_intel.py
@@ -5,4 +5,14 @@
 # AbuseIPDB  — IP abuse confidence scoring
 # ============================================================
 
-
+import re
+import time
+import base64
+import requests
+from config import (
+    VIRUSTOTAL_API_KEY,
+    VIRUSTOTAL_URL_SCAN,
+    VIRUSTOTAL_IP_SCAN,
+    ABUSEIPDB_API_KEY,
+    ABUSEIPDB_URL
+)
